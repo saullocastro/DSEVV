12353886a0e9f9a1e4ce1c6cbae19811ec97ae1b
saullocastro <saullogiovani@gmail.com>
2020-05-12 09:49:39 +0200

    DEV: added fuselage parameters

--- DSEVV/parameters.py
+++ DSEVV/parameters.py
@@ -8,12 +8,21 @@ class Wing(object):
         self.span = 20
         self.sweepdeg = 30
 
+class Fuselage(object):
+    #NOTE use __slots__ to avoid unintended mistakes
+    __slots__ = ['radius', 'length']
+    def __init__(self):
+        self.radius = 1.5
+        self.length = 30
+
 class Parameters(object):
-    __slots__ = ['wing']
+    __slots__ = ['wing', 'fuselage']
     def __init__(self):
         self.wing = Wing()
+        self.fuselage = Fuselage()
 
 # using dictionaries (key: value)
 wing = dict(coordroot=2, coordtip=1, span=20, sweepdeg=30)
-parameters_dict = dict(wing=wing)
+fuselage = dict(radius=1.5, length=30)
+parameters_dict = dict(wing=wing, fuselage=fuselage)
 
